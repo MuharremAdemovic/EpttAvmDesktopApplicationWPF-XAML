114962852c9a13f455017693b4bb44e4a9b657c3
Muharrem <47212492+MuharremAdemovic@users.noreply.github.com>
2020-04-14 23:41:30 +0300

    14/04/2020

---
+++ WPFePTT/WPFePTT/Image3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFePTT
+{
+    /// <summary>
+    /// Interaction logic for Image3.xaml
+    /// </summary>
+    public partial class Image3 : Page
+    {
+        public Image3()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- WPFePTT/WPFePTT/MainWindow.xaml.cs
+++ WPFePTT/WPFePTT/MainWindow.xaml.cs
@@ -23,40 +23,142 @@ namespace WPFePTT
         public MainWindow()
         {
             InitializeComponent();
+
+            Main1.Content = new Image1();
+            
         }
 
-        //private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
-        //{
-        //    ButtonCloseMenu.Visibility = Visibility.Visible;
-        //    ButtonOpenMenu.Visibility = Visibility.Collapsed;
-        //}
 
-        //private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
-        //{
-        //    ButtonCloseMenu.Visibility = Visibility.Collapsed;
-        //    ButtonOpenMenu.Visibility = Visibility.Visible;
-        //}
-
-        //private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    UserControl usc = null;
-        //    GridMain.Children.Clear();
-
-        //    switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
-        //    {
-        //        case "ItemHome":
-        //            usc = new UserControlHome();
-        //            GridMain.Children.Add(usc);
-        //            break;
-        //        case "ItemCreate":
-        //            usc = new UserControlCreate();
-        //            GridMain.Children.Add(usc);
-        //            break;
-        //        default:
-        //            break;
-        //    }
-        //}
 
        
+
+        
+
+
+       
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Main1.Content = new Image1();
+        }
+
+        private void RadioButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            Main1.Content = new Image2();
+        }
+        private void RadioButton_Click_2(object sender, RoutedEventArgs e)
+        {
+            Main1.Content = new Image3();
+        }
+
+        private void RadioButton_Click_3(object sender, RoutedEventArgs e)
+        {
+            Main1.Content = new Image4();
+        }
+
+        private void RadioButton_Click_4(object sender, RoutedEventArgs e)
+        {
+            Main1.Content = new Image5();
+        }
+
+        private void RadioButton_Click_5(object sender, RoutedEventArgs e)
+        {
+            Main1.Content = new Image6();
+        }
+
+        private void RadioButton_Click_6(object sender, RoutedEventArgs e)
+        {
+            Main1.Content = new Image7();
+        }
+
+        private void RadioButton_Click_7(object sender, RoutedEventArgs e)
+        {
+            Main1.Content = new Image8();
+        }
+
+        private void RadioButton_Click_8(object sender, RoutedEventArgs e)
+        {
+            Main1.Content = new Image10();
+        }
+
+
+        private void ListViewItem_MouseMove(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new Elektronik();
+        }
+
+        private void ListViewItem_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new Anne();
+
+        }
+
+        private void ListViewItem_MouseMove_2(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new Moda();
+        }
+
+        private void ListViewItem_MouseMove_3(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new Ev();
+
+        }
+
+        private void ListViewItem_MouseMove_4(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new Kozmetik();
+        }
+
+        private void ListViewItem_MouseMove_5(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new HobiDunyasi();
+        }
+
+        private void ListViewItem_MouseMove_6(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new Spor();
+        }
+
+        private void ListViewItem_MouseMove_7(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new Supermarket();
+        }
+
+        private void ListViewItem_MouseMove_8(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new Otomobil();
+        }
+
+        private void ListViewItem_MouseMove_9(object sender, MouseEventArgs e)
+        {
+            Main1.Content = false;
+            Main.Content = new Pet();
+        }
+
+
+
+
+
+
+        //private void ListViewItem_MouseMove_9(object sender, MouseEventArgs e)
+        //{
+        //    Main1.Content = false;
+        //    Main.Content = new Pet();
+        //}
+
+        //private void ListViewItem_MouseLeave_8(object sender, MouseEventArgs e)
+        //{
+        //    Main.Content = false;
+        //}
     }
 }
